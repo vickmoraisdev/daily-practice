f41a7ee363deae9be62ef9d33658d2166fc06001
Vitória Morais <vitoriamoraisfaria@gmail.com>
2026-08-04 17:10:36 -0300

    Criando uma API de exemplo

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 /*
-Instalando uma versão específica - Nesta aula, vimos como instalar 
-versões específicas de pacotes para garantir compatibilidade.
-
-npm install json-server@1.0.0-alpha.17 | depois do @, você coloca a versão que deseja.
+Criando uma API de exemplo - Nesta aula, criamos dados 
+para simular uma API, definindo recursos como produtos. 
+Utilizamos o json-server para simular a API e testamos 
+acessando os produtos via navegador, fazendo requisições 
+do tipo GET. Demonstrando como filtrar produtos por ID 
+e como simular o comportamento de uma API.
 */
