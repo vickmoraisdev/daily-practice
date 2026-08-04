fbab960e825fe832329a53bc19068460c401e4e0
Vitória Morais <vitoriamoraisfaria@gmail.com>
2026-08-04 16:32:23 -0300

    Instalando uma versão específica

---
+++ main.js
@@ -0,0 +1,6 @@
+/*
+Instalando uma versão específica - Nesta aula, vimos como instalar 
+versões específicas de pacotes para garantir compatibilidade.
+
+npm install json-server@1.0.0-alpha.17 | depois do @, você coloca a versão que deseja.
+*/
